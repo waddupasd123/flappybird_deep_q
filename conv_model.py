7b7a755125c725f5c6b28dfed71356302d501fbe
Andrew Chen <z5259171@ad.unsw.edu.au>
2022-11-10 00:35:41 +1100

    PAIN

--- conv_model.py
+++ conv_model.py
@@ -9,11 +9,18 @@ class FlappyConv(nn.Module):
         self.conv1 = nn.Conv2d(4, 32, kernel_size=4, stride=4)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
-        self.fc1(256 ,2)
+        self.fc1 = nn.Linear(256 ,2)
+        self._create_weights()
+
+    def _create_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+                nn.init.uniform_(m.weight, -0.01, 0.01)
+                nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
         x = F.max_pool2d(self.conv1(x), (2, 2))
-        x - F.max_pool2d(self.con2(x), (2, 2))
+        x - F.max_pool2d(self.conv2(x), (2, 2))
         x = F.max_pool2d(self.conv3(x), (2, 2))
         x = F.relu(torch.flatten(x, 1))
         x = self.fc1(x)
--- deep_q_train.py
+++ deep_q_train.py
@@ -1,15 +1,129 @@
-import numpy as np
 import torch
 import torch.nn as nn
+import numpy as np
+import cv2
+import flappy_mod as Flappy
 from conv_model import FlappyConv
+from random import random, randint, sample
+import matplotlib.pyplot as plt
+
+INITIAL_EPSILON = 0.1       # starting value of epsilon
+FINAL_EPSILON = 0.0001      # final value of epsilon
+NUM_ITERS = 2000000
+REPLAY_MEMORY_SIZE = 50000
+BATCH_SIZE = 32
+GAMMA = 0.99
+LEARNING_RATE = 0.000006
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def train():
     model = FlappyConv()
 
-    if torch.cuda.is_available():  
-        dev = "cuda:0"
-        model.cuda()
-        image = image.cuda() 
-    else:  
-        dev = "cpu"  
+    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    criterion = nn.MSELoss()
 
+    Flappy.launch()
+    movementInfo = Flappy.initialSetup()
+    gameInfo = Flappy.mainGameSetup(movementInfo)
+    
+    image, gameInfo, death = Flappy.mainGame(gameInfo)
+    image = cv2.cvtColor(cv2.resize(image, (80, 80)), cv2.COLOR_BGR2GRAY)
+    image = np.reshape(image, (80, 80, 1))
+
+    #state = np.stack((image, image, image, image), axis=2)
+
+    image = torch.from_numpy(image)
+    model.to(device)
+    image = image.to(device)
+
+    state = torch.cat(tuple(image for _ in range(4)))[None, :, :, :]
+
+    replay_memory = []
+    iter = 0
+    score = 0
+    while iter < NUM_ITERS:
+        prediction = model(state)[0]
+        # Exploration or exploitation
+        epsilon = FINAL_EPSILON + ((NUM_ITERS - iter) * (INITIAL_EPSILON - FINAL_EPSILON) / NUM_ITERS)
+        u = random()
+        random_action = u <= epsilon
+        if random_action:
+            print("Perform a random action")
+            action = randint(0, 1)
+        else:
+            action = torch.argmax(prediction)[0]
+
+        quit = Flappy.action(action)
+        next_image, gameInfo, death = Flappy.mainGame(gameInfo)
+        if death:
+            reward = -1
+        elif gameInfo['score'] > score:
+            reward = 1
+            score = gameInfo['score']
+        else: 
+            reward = 0.1
+
+        next_image = cv2.cvtColor(cv2.resize(next_image, (80, 80)), cv2.COLOR_BGR2GRAY)
+        next_image = np.reshape(next_image, (80, 80, 1))
+        next_image = torch.from_numpy(next_image)
+        next_image = next_image.to(device)
+        next_state = torch.cat((state[0, 1:, :, :], next_image))[None, :, :, :]
+
+        replay_memory.append([state, action, reward, next_state, death])
+        if len(replay_memory) > REPLAY_MEMORY_SIZE:
+            del replay_memory[0]
+        batch = sample(replay_memory, min(len(replay_memory), BATCH_SIZE))
+        state_batch, action_batch, reward_batch, next_state_batch, terminal_batch = zip(*batch)
+
+        state_batch = torch.cat(tuple(state for state in state_batch))
+        action_batch = torch.from_numpy(
+            np.array([[1, 0] if action == 0 else [0, 1] for action in action_batch], dtype=np.float32))
+        reward_batch = torch.from_numpy(np.array(reward_batch, dtype=np.float32)[:, None])
+        next_state_batch = torch.cat(tuple(state for state in next_state_batch))
+
+        if torch.cuda.is_available():
+            state_batch = state_batch.to(device)
+            action_batch = action_batch.to(device)
+            reward_batch = reward_batch.to(device)
+            next_state_batch = next_state_batch.to(device)
+        current_prediction_batch = model(state_batch)
+        next_prediction_batch = model(next_state_batch)
+
+        y_batch = torch.cat(
+            tuple(reward if terminal else reward + GAMMA * torch.max(prediction) for reward, terminal, prediction in
+                  zip(reward_batch, terminal_batch, next_prediction_batch)))
+
+        q_value = torch.sum(current_prediction_batch * action_batch, dim=1)
+        optimizer.zero_grad()
+        # y_batch = y_batch.detach()
+        loss = criterion(q_value, y_batch)
+        loss.backward()
+        optimizer.step()
+
+        state = next_state
+        iter += 1
+        print("Iteration: {}/{}, Action: {}, Loss: {}, Epsilon {}, Reward: {}, Q-value: {}".format(
+            iter + 1,
+            NUM_ITERS,
+            action,
+            loss,
+            epsilon, reward, torch.max(prediction)))
+        #writer.add_scalar('Train/Loss', loss, iter)
+        plt.figure(1)
+        plt.title("Train/Loss")
+        plt.ylabel("Train")
+        plt.ylabel("Loss")
+        plt.plot(loss, iter)
+        plt.show(block=False)
+        plt.pause(0.001)
+        #writer.add_scalar('Train/Epsilon', epsilon, iter)
+        #writer.add_scalar('Train/Reward', reward, iter)
+        #writer.add_scalar('Train/Q-value', torch.max(prediction), iter)
+        if (iter+1) % 1000000 == 0:
+            torch.save(model, "data/data" + str(iter + 1))
+    torch.save(model, "data/data")
+
+
+if __name__ == "__main__":
+    train()
